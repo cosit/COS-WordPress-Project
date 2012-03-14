eaa9a638e109e2a5457649934a0a88d264f65639
Jonathan Hendricker <jonathan.hendricker@ucf.edu>
2012-03-14 11:50:31 -0400

    folder removal

--- COS/searchform.php
+++
@@ -1,8 +0,0 @@
-<form action="<?php bloginfo('siteurl'); ?>" id="searchform" method="get">
-    
-        <label for="s" class="screen-reader-text">Search for:</label>
-        <input type="submit" value="" class="searchsubmit"  style="vertical-align:middle;" />
-        <input type="text" id="s" name="s" size="25" value="Search College of Sciences" onfocus="this.value=''" onblur="this.value='Search College of Sciences'" />
-                
-    
-</form>
