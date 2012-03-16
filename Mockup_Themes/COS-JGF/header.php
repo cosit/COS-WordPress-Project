f7e88f77bc42a023e6734c016343808472b5b9f7
Jesse Ferraro <jesse.ferraro@ucf.edu>
2012-03-16 13:23:54 -0400

    The rest of these files are copied from Jonathan's theme version

---
+++ Mockup_Themes/COS-JGF/header.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
+
+<head profile="http://gmpg.org/xfn/11">
+	
+	<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
+	
+	<?php if (is_search()) { ?>
+	   <meta name="robots" content="noindex, nofollow" /> 
+	<?php } ?>
+
+	<title>
+		   <?php
+		      if (function_exists('is_tag') && is_tag()) {
+		         single_tag_title("Tag Archive for &quot;"); echo '&quot; - '; }
+		      elseif (is_archive()) {
+		         wp_title(''); echo ' Archive - '; }
+		      elseif (is_search()) {
+		         echo 'Search for &quot;'.wp_specialchars($s).'&quot; - '; }
+		      elseif (!(is_404()) && (is_single()) || (is_page())) {
+		         wp_title(''); echo ' - '; }
+		      elseif (is_404()) {
+		         echo 'Not Found - '; }
+		      if (is_home()) {
+		         bloginfo('name'); echo ' - '; bloginfo('description'); }
+		      else {
+		          bloginfo('name'); }
+		      if ($paged>1) {
+		         echo ' - page '. $paged; }
+		   ?>
+	</title>
+	
+	<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+	
+	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" />
+	
+	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+
+	<?php if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); ?>
+
+	<?php wp_head(); ?>
+	
+</head>
+
+<body <?php body_class(); ?>>
+   
+	<div id="page-wrap">
+
+		<!-- ADD UCF HEADER -->
+		<script type="text/javascript" src="http://universityheader.ucf.edu/bar/js/university-header.js"></script>
+		<!-- END UCF HEADER -->
+
+		<div id="header">
+			<h1>UCF<a href="<?php echo get_option('home'); ?>/"><?php bloginfo('name'); ?></a></h1>
+			<?php /* <div class="description"><?php bloginfo('description'); ?></div> //Descriptiono not used */?> 
+            
+            <!--Custom Nav-->
+			<nav id="access" role="navigation">
+				<?php /* Our navigation menu.  If one isn't filled out, wp_nav_menu falls back to wp_page_menu. The menu assiged to the primary position is the one used. If none is assigned, the menu with the lowest ID is used. */ ?>
+                <?php wp_nav_menu( array( 'theme_location' => 'main', 'menu_class' => 'headerNav', 'sort_column' => 'menu_order') ); ?>
+			</nav>
+			<!-- End Custom Nav-->
+		</div>
+
+
