ff3d31cbf8d45ce924a5bc6f63b9db96d44cce13
Jesse Ferraro <jesse.ferraro@ucf.edu>
2012-05-03 09:40:02 -0400

    Jonathan's Search File

--- Mockup_Themes/COS-dk/search.php
+++ Mockup_Themes/COS-dk/search.php
@@ -14,15 +14,25 @@ get_header(); ?>
 		<?php if (function_exists('breadcrumbs')) breadcrumbs(); ?>
 		<?php get_search_form(); ?>
 		<div id="search_results" class="innerContent">
-<?php if ( have_posts() ) : ?>
-		<h1><?php printf( __( 'Search Results for: <span>%s</span>', 'starkers' ), '' . get_search_query() . '' ); ?></h1>
+
+		<?php if (have_posts()) : ?>
+			<h1><?php printf( __( 'Search Results for: <span>%s</span>', 'starkers' ), '' . get_search_query() . '' ); ?></h1>
+			<?php while (have_posts()) : the_post(); ?>
+				<div <?php post_class() ?> id="post-<?php the_ID(); ?>">
+					<h2><a href="?page_id=<?php the_ID();?>"><?php the_title(); ?></a></h2>			
+					<div class="entry">
+						<?php the_excerpt(); ?>
+					</div>
+				</div>
+			<?php endwhile; ?>
+	
 			<?php
 				get_template_part( 'loop', 'search' );
 			?>
-<?php else : ?>
+		<?php else : ?>
 		<h1><?php printf( __( 'Nothing Found for: <span>%s</span>', 'starkers' ), '' . get_search_query() . ''); ?></h1>
 			<h2><?php _e( 'Sorry, but nothing matched your search criteria. Please try again with some different keywords.', 'starkers' ); ?></h2>
-<?php endif; ?>
+		<?php endif; ?>
 		</div> <!-- End innerContent -->
 	</div> <!-- End Wrap -->
 </section>
