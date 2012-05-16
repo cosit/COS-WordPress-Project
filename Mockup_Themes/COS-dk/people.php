08befb16bfc144c4f334e7ad90033302db935180
Jesse Ferraro <jesse.ferraro@ucf.edu>
2012-05-16 15:53:58 -0400

    Added people navigation in

--- Mockup_Themes/COS-dk/people.php
+++ Mockup_Themes/COS-dk/people.php
@@ -25,10 +25,9 @@ Template Name: People List
 				</footer>
 			</article>
 		</div>
-		
 		<div id="sidebar">
-		<?php people_nav(); ?>
-		</div>	
+			<?php people_nav(); ?>
+		</div>
 <?php endwhile; ?>
 	</div> <!-- End Wrap -->
 </section>
