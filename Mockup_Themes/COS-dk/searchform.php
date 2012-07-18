055f3feb322bbc3fd15fa30354832963256fd42d
David Khourshid <cosweb@ucf.edu>
2012-07-18 14:00:02 -0400

    Most changes (Jonathan) compiled, new "specialty" subheader support, tabs still need work"

--- Mockup_Themes/COS-dk/searchform.php
+++
@@ -1,5 +0,0 @@
-<form role="search" method="get" id="<?php if(!is_front_page()) echo 'inner_'; ?>searchform" action="<?php echo home_url( '/' ); ?>" >
-    <div>
-        <input type="text" value="Search <?php bloginfo( 'name' ); ?>..." name="s" id="s" onfocus="this.value=(this.value=='Search <?php bloginfo( 'name' ); ?>...') ? '' : this.value;" onblur="this.value=(this.value=='') ? 'Search <?php bloginfo( 'name' ); ?>...' : this.value;"/>
-    </div>
-</form>
