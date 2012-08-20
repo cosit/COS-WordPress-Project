aa642c64c8b0b3eb009c4689ef357e379e5c48f4
cosit <cosit@ucf.edu>
2012-08-20 14:05:39 -0300

    Update Mockup_Themes/COS-dk/taxonomy-people_cat.php
    For the Theme's "Sidebar Location option" to work the sidebar div needs to be located above the content.

--- Mockup_Themes/COS-dk/taxonomy-people_cat.php
+++ Mockup_Themes/COS-dk/taxonomy-people_cat.php
@@ -13,17 +13,15 @@ get_header(); ?>
 	<div class="wrap clearfix">
 	<?php if (function_exists('breadcrumbs')) breadcrumbs(); ?>
 	<?php get_search_form(); ?>
-
+		<div id="sidebar" style="float: <?php echo get_option('COS_sidebar_location');?>;">
+			<?php people_nav( get_query_var('page_id') ); ?>
+			<?php get_sidebar(); ?>
+		</div> 
 		<div class="innerContent">
 			<h1>People <span>&raquo;</span> <?php echo ucwords( preg_replace("/-/", " ", $_GET['people_cat'] ) ); ?> </h1>
 			<?php show_people( $_GET['people_cat'] ); ?>
 		</div>
-
-		<div id="sidebar" style="float: <?php echo get_option('COS_sidebar_location');?>;">
-		<?php people_nav( get_query_var('page_id') ); ?>
-		<?php get_sidebar(); ?>
-		</div> 
-
+	
 	</div> <!-- End Wrap -->
 </section>
 
