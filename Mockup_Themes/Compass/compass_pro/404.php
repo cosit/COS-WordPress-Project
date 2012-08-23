7627b082912e945a91734fe47738ef8cd3704cb8
Jonathan Hendricker <jonathan.hendricker@ucf.edu>
2012-08-23 11:08:36 -0400

    Addition of COMPASS Theme and Plugin

---
+++ Mockup_Themes/Compass/compass_pro/404.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * The template for displaying 404 pages.
+ *
+ * @package WordPress
+ * @subpackage Starkers
+ * @since Starkers HTML5 3.0
+ */
+
+get_header(); ?>
+
+<section id="main_content">
+	<div class="wrap clearfix">
+	<?php if (function_exists('breadcrumbs')) breadcrumbs(); ?>
+	<?php get_search_form(); ?>
+		<h1><?php _e( 'Not Found', 'starkers' ); ?></h1>
+			<h2><?php _e( 'Apologies, but the page you requested could not be found. Perhaps searching will help.', 'starkers' ); ?></h2>
+
+
+		<script type="text/javascript">
+			// focus on search field after it has loaded
+			document.getElementById('s') && document.getElementById('s').focus();
+		</script>
+	</div>
+</section>
+
+<?php get_footer(); ?>
